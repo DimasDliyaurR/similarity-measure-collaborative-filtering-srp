f4dbf95e40b1a742138144b6354458554c1f2316
Dimas_Dr <dimasdliyaurrahman@gmail.com>
2024-10-04 02:58:57 +0700

    [FIX] Kelas Prediction,Similarity,dan MeanCentered ,[ADD] Dokumentasi Setiap Kelas

--- helper/helper.py
+++ helper/helper.py
@@ -1,10 +1,17 @@
 def indexOfZero (data1,data2):
     return [i for i in range(len(data2)) if data1[i] == 0 or data2[i] == 0]
 
-def checkIndexZeroOfData (data,index,length):
-    print("Data :",data[index][0:length])
-    return [i for i in range(len(data[index][0:length])) if data[index][i] == 0]
-    
+def checkIndexZeroOfData (*,data,index,indexUser):
+    result = [i for i in range(len(data[index])) if data[index][i] == 0]
+    if not heyStack(indexUser,result) :
+        result.append(indexUser)
+    return result
+
+def heyStack(needle,haystack) :
+    for item in haystack :
+        if item == needle :
+            return True
+    return False
 
 def reverseMatrix(data) :
     return [[float(data[j][i]) for j in range(len(data))]for i in range(len(data[0]))]
@@ -14,7 +21,7 @@ def remove_items(test_list, item):
     res = [i for i in test_list if i != item] 
     return res
  
-def isExistIndex(index,data): 
+def isNotExistIndex(index,data): 
     return 0 <= index < len(data)
 
 def createList(r1, r2):
